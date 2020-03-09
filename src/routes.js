4032a04601e0e88c4dec928a35321f327aa86258
felipeWanderson <felipewanderson2020@gmail.com>
2020-03-08 23:01:16 -0300

    feat:Autenticação - cria usuário

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Felipe Leal',
+    email: 'felipe2@fastfeet.com',
+    password_hash: '121212121212',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
