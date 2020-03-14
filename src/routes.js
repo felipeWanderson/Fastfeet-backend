b4ab5a9156539963069da4bf257f35025ea35ce3
felipeWanderson <felipewanderson2020@gmail.com>
2020-03-14 18:20:16 -0300

    feat:cadastro de usuario

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Felipe Leal',
-    email: 'felipe2@fastfeet.com',
-    password_hash: '121212121212',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
