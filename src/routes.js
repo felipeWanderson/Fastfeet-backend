84443104c33e2e125ada11e5a34f5a27f42ffa54
felipeWanderson <felipewanderson2020@gmail.com>
2020-03-16 23:01:32 -0300

    feat: add relacionamento de deliveryman e files

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,33 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
 routes.get('/deliverymans', DeliverymanController.index);
 routes.post('/deliverymans', DeliverymanController.store);
 routes.put('/deliverymans/:id', DeliverymanController.update);
 routes.delete('/deliverymans/:id', DeliverymanController.delete);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 import app from './app';
 
-app.listen(2222);
+app.listen(3333);
